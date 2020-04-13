de487e772b330c97c9584d7e4ced26824b4815fa
phpervip <2998658517@qq.com>
2020-04-13 22:27:45 +0800

    完善ffmpeg批处理

---
+++ ffmpeg/02/f02readme.txt
@@ -0,0 +1,8 @@
+1.先把mp4_ffprobe.bat,mp4_txt_list.bat,get_json_info_all.py,get_json_info.py放在要处理的目录下。
+2.执行mp4_ffprobe.bat,（windows可双击） 
+  在mp4info目录下生成对应的mp4信息文件.txt
+3.执行mp4_txt_list.bat（windows可双击）
+  生成mp4_txt_list.txt即这些信息文件的清单
+4.执行get_json_info_all.py
+  根据这批清单生成excel表文件。
+  生成mp4_txt_info.csv
--- ffmpeg/02/mp4_ffprobe.bat
+++ ffmpeg/02/mp4_ffprobe.bat
@@ -3,7 +3,16 @@
 ::设置要获取视频信息的文件
 
 set Video=H:\users\dos\ffmpeg\demo3.mp4
-set savePath=H:\users\dos\ffmpeg\mp4info\
+
+set /p savePath=Please enter the file directory to process(default is F:\mp4\mp4info\)：
+if "%savePath%"=="" set Dir="F:\mp4\mp4info\"
+
+if exist %savePath% (
+    echo %savePath% is exist
+) else (
+   echo create %savePath%
+   md %savepath%
+)
 
 ::获取目录下及子目录下所有的mp4文件路径
 ::(for /r %%i in (*.mp4) do echo %%i) >mp4_list.txt
@@ -18,9 +27,10 @@ for /r %%i in (*.mp4) do (
 ::echo %%i
 set /a n+=1&set "str=00000!n!"
 set video=%%i
-set filename=!video:~-15!
+::set filename=!video:~-15!
 set filename=%%~nxi
 echo !str:~-6!
 ::echo %%~nxi
 ffprobe.exe -v quiet -print_format json -show_format -show_streams !Video! >!savePath!!filename!.txt 2>&1
 ) 
+pause
